10e4d67b9790f86efc550d247c4e28a6948dbb9a
ST1414 <sterry1414@gmail.com>
2021-12-03 10:36:29 -0800

    Updated index.js and server.js so that the server is up and running successfully

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const server = express();
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.use(express.json());
+// server.use('/api/projects', projectsRouter);
+// server.use('/api/actions', actionsRouter);
+
+server.get('*', (req, res) => {
+    res.status(200).send('<h1>Hooray! You found us!!!</h1>')
+})
+
 
 module.exports = server;
