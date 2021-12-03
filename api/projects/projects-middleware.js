fe9c7083146a10e0bfba0510173a3b5e67c26a28
ST1414 <sterry1414@gmail.com>
2021-12-03 13:52:37 -0800

    completed and tested get all, get by id, and post new project

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -2,9 +2,11 @@
 const Project = require('./projects-model');
 
 function validateProjectID (req, res, next){
+        // - If there is no project with the given `id` it responds with a status code 404.
     Project.get(req.params.id)
         .then( response => {
             if (response) {
+                req.project = response
                 next();
             } else {
                 res.status(404).json({ error: `No project with ID ${req.params.id} found` })
@@ -16,6 +18,12 @@ function validateProjectID (req, res, next){
 }
 
 function validateProject (req, res, next){
-    console.log('\n* MW: Validating Project *')
-    next();
+    // - If the request body is missing any of the required fields it responds with a status code 400.
+    if (!req.body.name || !req.body.description || !req.body.completed){
+        res.status(400).json({ error: `Name, description, and completed fields required` })
+    } else {
+        next();
+    }
 }
+
+module.exports = { validateProjectID, validateProject }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,16 +1,17 @@
 // ----- Imports -----
 const express = require('express');
 const Project = require('./projects-model');
-// const {  } = require('./projects-middleware');
+const { validateProjectID, validateProject } = require('./projects-middleware');
 
 
+//throw new Error("!!! KABOOM !!!")
 // ----- Set up Router and Endpoints -----
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    // - Returns an array of projects as the body of the response.
-    // - If there are no projects it responds with an empty array.    
-    Project.get(req.null)
+    // x Returns an array of projects as the body of the response. 
+    // x If there are no projects it responds with an empty array.    
+    Project.get()
     .then( response => {
         res.status(200).json(response)
     })
@@ -20,32 +21,43 @@ router.get('/', (req, res) => {
 
 })
 
-router.get('/:id', (req, res) => {
-    // - Returns a project with the given `id` as the body of the response.
-    // - If there is no project with the given `id` it responds with a status code 404.
-    res.status(200).json({ message: 'GET Project by ID'});
+router.get('/:id', validateProjectID, (req, res) => {
+    // x Returns a project with the given `id` as the body of the response.
+    // x If there is no project with the given `id` it responds with a status code 404.
+    res.status(200).json(req.project)
+
 })
 
-router.post('/', (req, res) => {
-    // - Returns the newly created project as the body of the response.
-    // - If the request body is missing any of the required fields it responds with a status code 400.
-    console.log('\nPOST new project');
-    res.status(200).json({ message: 'POST new project'});
-    
+router.post('/', validateProject, (req, res) => {
+    // x Returns the newly created project as the body of the response.
+    // x If the request body is missing any of the required fields it responds with a status code 400.
+    Project.insert(req.body)
+    .then( response => {
+            res.status(201).json(response);
+        })
+        .catch( err => {
+            res.status(500).json({ message: err.message});
+        })
 })
 
-router.put('/:id', (req, res) => {
+
+// ***** WORKING *****
+router.put('/:id', validateProjectID, validateProject, (req, res) => {
     // - Returns the updated project as the body of the response.
     // - If there is no project with the given `id` it responds with a status code 404.
     // - If the request body is missing any of the required fields it responds with a status code 400.
     res.status(200).json({ message: 'PUT update a project'});
 })
 
+
+
 router.delete('/:id', (req, res) => {
     // - Returns no response body.
     // - If there is no project with the given `id` it responds with a status code 404.
     res.status(200).json({ message: 'DELETE a project'});
 })
+
+
 
 router.get('/:id/actions', (req, res) => {
     // - Returns an array of actions (could be empty) belonging to a project with the given `id`.
